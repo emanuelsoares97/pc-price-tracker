459b85616065f45423865eec6625aec109f90cd6
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-25 20:35:06 +0000

    Update comparador_main.py

--- scripts/comparador_main.py
+++ scripts/comparador_main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Adiciona o diretório raiz do projeto ao sys.path
+# diretório raiz do projeto
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
