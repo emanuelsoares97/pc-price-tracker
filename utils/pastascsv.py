a71ca62184c8a64f86dcc3054812df5fd74b8db9
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-26 12:41:58 +0000

    Update pastascsv.py

--- utils/pastascsv.py
+++ utils/pastascsv.py
@@ -3,6 +3,6 @@ import os
 
 caminho_projeto = os.path.dirname(os.path.abspath(__file__))
 
-caminho_csv_diario = os.path.join(caminho_projeto, "..", "data/csv")
+caminho_csv_diario = os.path.join(caminho_projeto, "..", "data/raw")
 
-caminho_csv_analise = os.path.join(caminho_projeto, "..", "data/analisecsv")
+caminho_csv_analise = os.path.join(caminho_projeto, "..", "data/reports")
