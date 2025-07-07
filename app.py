00e83537215d4c4928cd6a5eafae0b87879a398f
emanuelsoares97 <tutasoares17@gmail.com>
2025-07-07 18:58:53 +0100

    atualizada rota para download

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
 import traceback
 from scripts import scraper_main, comparador_main
 from utils.logger_util import get_logger
+import os
 
 app = Flask(__name__)
 logger = get_logger(__name__)
@@ -25,6 +26,16 @@ def scrap():
         logger.error(f"Erro: {e}")
         traceback.print_exc()
         return jsonify({'status': 'erro', 'mensagem': str(e)}), 500
+
+@app.route('/download/raw/<filename>')
+def download_raw(filename):
+    """Permite baixar arquivos CSV da pasta data/raw."""
+    return send_from_directory(os.path.join('data', 'raw'), filename, as_attachment=True)
+
+@app.route('/download/reports/<filename>')
+def download_reports(filename):
+    """Permite baixar arquivos CSV da pasta data/reports."""
+    return send_from_directory(os.path.join('data', 'reports'), filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True) 
